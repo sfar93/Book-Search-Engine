01682815e32749511f92a28297ea61c2df333485
saleh farah <salehf1993@gmail.com>
2025-04-15 21:36:43 -0700

    fixed final code

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,6 +4,9 @@ import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server'; // Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './Schemas/index.js';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import { authenticateToken } from './services/auth.js';
 const server = new ApolloServer({
     typeDefs,
@@ -19,12 +22,12 @@ const startApolloServer = async () => {
     app.use('/graphql', expressMiddleware(server, {
         context: authenticateToken
     }));
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
-        app.get('*', (_req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-        });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.get('*', (_req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
+    // }
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,9 @@ import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './Schemas/index.js';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import { authenticateToken } from './services/auth.js';
 
 const server = new ApolloServer({
@@ -29,10 +32,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
